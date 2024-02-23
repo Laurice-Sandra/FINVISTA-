4a67371f17801e2088fb80bf7c5df055a6960cd9
Laurice-Sandra <lauricesandra.kouamtchuendem@esprit.tn>
2024-02-23 18:29:10 +0100

    first commit

---
+++ Backend/FlexiFin/src/main/java/esprit/tn/flexifin/FlexiFinApplication.java
@@ -0,0 +1,13 @@
+package esprit.tn.flexifin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexiFinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexiFinApplication.class, args);
+    }
+
+}
