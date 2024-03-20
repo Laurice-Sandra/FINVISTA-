ec63510774679a28cf713f9cb7f83c029fd24df5
Laurice-Sandra <lauricesandra.kouamtchuendem@esprit.tn>
2024-03-20 09:12:41 +0100

    start lpay

---
+++ Backend/FlexiFin/src/main/java/esprit/tn/flexifin/repositories/TransactionRepository.java
@@ -0,0 +1,4 @@
+package esprit.tn.flexifin.repositories;
+
+public interface TransactionRepository extends org.springframework.data.jpa.repository.JpaRepository<esprit.tn.flexifin.entities.Transaction, java.lang.Long> {
+}
